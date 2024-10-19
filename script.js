5365989c04aa1db7b31e8b6acac4dfdf36d15522
quang.do <doquang120198@gmail.com>
2024-10-19 23:13:43 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const message = document.querySelector(".message");
+
+  setTimeout(() => {
+    message.innerHTML =
+      "Cảm ơn em đã cho anh cơ hội quan tâm, lo lắng cho em. Đối với anh , em là sự ưu tiên chứ không phải sự lựa chọn.Nếu em cần, a sẽ luôn ở đây để lắng nghe em ❤";
+  }, 5000);
+});
